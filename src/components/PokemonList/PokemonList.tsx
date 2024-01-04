8b0a357e7608779c8498f2423102cd22d77b4bcc
seatbelts <is.ma95@hotmail.com>
2024-01-04 13:38:15 -0600

    refactor: improved typings with library

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
-import {
-  PokemonListI,
-  PokemonListResponseI,
-} from '../../interfaces/pokemon-list.interface';
+import PokemonCard from '../PokemonCard/PokemonCard';
+import { NamedAPIResource, NamedAPIResourceList } from 'pokenode-ts';
 
 const PokemonList = () => {
-  const [pokemonList, setPokemonList] = useState([] as PokemonListI[]);
+  const [pokemonList, setPokemonList] = useState([] as NamedAPIResource[]);
   const [nextUrl, setNextUrl] = useState<string>('');
   const [previousUrl, setPreviousUrl] = useState<string>('');
   const [scroll, setScroll] = useState<boolean>(false);
@@ -14,24 +12,21 @@ const PokemonList = () => {
   useEffect(() => {
     void fetchPokemonList();
 
-    function fetchPokemonList(): void {
-      axios
-        .get<PokemonListResponseI>(
-          'https://pokeapi.co/api/v2/pokemon/?limit=40'
-        )
-        .then(({ data }: AxiosResponse<PokemonListResponseI>) => {
-          setPokemonList(data.results);
-        })
-        .catch((error) => console.error(error));
+    async function fetchPokemonList(): Promise<void> {
+      const { data }: AxiosResponse<NamedAPIResourceList> =
+        await axios.get<NamedAPIResourceList>(
+          'https://pokeapi.co/api/v2/pokemon/?limit=20'
+        );
+      setPokemonList(data.results);
     }
   }, [scroll]);
 
   return (
     <div>
       {
-        <div>
-          {pokemonList.map((pokemon) => {
-            return <p key={pokemon.name}>{pokemon.name}</p>;
+        <div className="grid grid-cols-3 gap-3">
+          {pokemonList.map((pokemon: NamedAPIResource, index: number) => {
+            return <PokemonCard key={index} pokemon={pokemon} />;
           })}
         </div>
       }
