272c39849b0324d73d0dcf204139ee067126898b
seatbelts <is.ma95@hotmail.com>
2024-01-04 16:37:03 -0600

    feat: added title header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import PokemonList from './components/PokemonList/PokemonList';
 import PokemonDetails from './components/PokemonDetails/PokemonDetails';
 
@@ -8,6 +8,9 @@ function App() {
   return (
     <div style={{ backgroundImage: `url(/bg.png)` }}>
       <BrowserRouter>
+        <header className="container text-center text-6xl rounded-lg bg-slate-50 w-1/4">
+          <Link to="/">Pokedex</Link>
+        </header>
         <Routes>
           <Route path="/" element={<PokemonList />} />
           <Route path="/details/:pokemonName" element={<PokemonDetails />} />
