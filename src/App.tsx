3254cf7795a54b5c81c066e0072f2bd50391987d
seatbelts <is.ma95@hotmail.com>
2024-01-04 12:32:35 -0600

    feat: added basic functionality for list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PokemonList from './components/PokemonList/PokemonList';
@@ -8,25 +7,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/list" element={<PokemonList />} />
+        <Route path="/" element={<PokemonList />} />
       </Routes>
     </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
