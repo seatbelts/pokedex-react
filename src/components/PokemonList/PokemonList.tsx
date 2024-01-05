d08585d0b1cf6febf86199963a692bc67411c112
seatbelts <is.ma95@hotmail.com>
2024-01-04 23:16:25 -0600

    feat: added infinite scroll

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,40 +1,92 @@
 import { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import PokemonCard from '../PokemonCard/PokemonCard';
-import { NamedAPIResource, NamedAPIResourceList } from 'pokenode-ts';
+import { NamedAPIResource, NamedAPIResourceList, Pokemon } from 'pokenode-ts';
 import { Link } from 'react-router-dom';
 
+import { PokemonListI } from '../../interfaces/pokemon-list.interface';
+
 const PokemonList = () => {
-  const [pokemonList, setPokemonList] = useState([] as NamedAPIResource[]);
+  const [pokemonList, setPokemonList] = useState([] as PokemonListI[]);
   const [nextUrl, setNextUrl] = useState<string>('');
-  const [previousUrl, setPreviousUrl] = useState<string>('');
-  const [scroll, setScroll] = useState<boolean>(false);
+  const [totalCount, setTotalCount] = useState<number>(100);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchPokemonDetails(
+    pokemons: NamedAPIResourceList
+  ): Promise<Pokemon[]> {
+    return Promise.all(
+      pokemons.results.map(async (results: NamedAPIResource) => {
+        const { data }: AxiosResponse<Pokemon> = await axios.get<Pokemon>(
+          results.url
+        );
+        return data;
+      })
+    );
+  }
+
+  async function fetchPokemonList(
+    url = 'https://pokeapi.co/api/v2/pokemon/?limit=12'
+  ): Promise<void> {
+    const { data }: AxiosResponse<NamedAPIResourceList> =
+      await axios.get<NamedAPIResourceList>(url);
+    const pokemons = await fetchPokemonDetails(data);
+
+    const details: PokemonListI[] = data.results.map(
+      (result: NamedAPIResource, index: number) => {
+        return {
+          ...result,
+          details: pokemons[index],
+        };
+      }
+    );
+    setNextUrl(data.next ?? '');
+    setPokemonList((current) => {
+      return [...current, ...details];
+    });
+
+    setIsLoading(false);
+
+    if (totalCount === 100) {
+      setTotalCount(data.count);
+    }
+  }
+
+  window.onscroll = () => {
+    if (pokemonList.length > totalCount) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight &&
+      !isLoading
+    ) {
+      void fetchPokemonList(nextUrl);
+    }
+  };
 
   useEffect(() => {
     void fetchPokemonList();
-
-    async function fetchPokemonList(): Promise<void> {
-      const { data }: AxiosResponse<NamedAPIResourceList> =
-        await axios.get<NamedAPIResourceList>(
-          'https://pokeapi.co/api/v2/pokemon/?limit=20'
-        );
-      setPokemonList(data.results);
-    }
-  }, [scroll]);
+  }, []);
 
   return (
-    <div className="container">
-      {
+    <div className="container min-h-screen">
+      {isLoading ? (
+        <div className="items-center justify-center flex p-1">
+          <h2 className=" text-9xl animate-spin">🌀</h2>
+        </div>
+      ) : (
         <div className="grid grid-cols-3 gap-3">
-          {pokemonList.map((pokemon: NamedAPIResource, index: number) => {
+          {pokemonList.map((pokemon: PokemonListI, index: number) => {
             return (
-              <Link key="details" to={`/details/${pokemon.name}`}>
+              <Link key={index} to={`/details/${pokemon.name}`}>
                 <PokemonCard key={index} {...pokemon} />
               </Link>
             );
           })}
         </div>
-      }
+      )}
     </div>
   );
 };
